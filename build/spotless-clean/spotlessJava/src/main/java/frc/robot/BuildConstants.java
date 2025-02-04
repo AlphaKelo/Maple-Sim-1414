cfab8d6ab57cf18f5fa69140cd8eef3f3e0a0653
AlphaKelo <Kdragoon210@gmail.com>
2025-02-04 11:11:04 -0500

    Initial commit

---
+++ build/spotless-clean/spotlessJava/src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+    public static final String MAVEN_GROUP = "";
+    public static final String MAVEN_NAME = "FRC1414-2025-Code";
+    public static final String VERSION = "unspecified";
+    public static final int GIT_REVISION = 24;
+    public static final String GIT_SHA = "136ae47c1ca67a9919ecc0415c3b8720e7251579";
+    public static final String GIT_DATE = "2025-02-01 11:08:04 EST";
+    public static final String GIT_BRANCH = "Maple-Sim";
+    public static final String BUILD_DATE = "2025-02-03 13:27:22 EST";
+    public static final long BUILD_UNIX_TIME = 1738607242683L;
+    public static final int DIRTY = 1;
+
+    private BuildConstants() {}
+}
